ec2589eedc45e98463b742c56efdc7c5011beca5
Ian Harris <ian@teasel.org>
2022-03-01 14:50:44 +0000

    add version

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM node:16.13.0 as build-stage
 ARG MODE="production"
 ARG AUTH0_CLIENTID="Please set the AUTH0_CLIENTID build ARG"
+ARG VERSION="0.0.0"
+ARG SHA1=""
+ARG BUILD_DATE=""
 WORKDIR /app
 RUN echo "VUE_APP_AUTH0_CLIENTID=$AUTH0_CLIENTID" | tee .env.local
 COPY package*.json ./
@@ -12,3 +15,6 @@ FROM nginx:alpine as production-stage
 RUN mkdir /app
 COPY --from=build-stage /app/dist /app
 COPY nginx.conf /etc/nginx/nginx.conf
+ENV VUE_APP_VERSION=$VERSION
+ENV VUE_APP_SHA1=$SHA1
+ENV VUE_APP_BUILD_DATE=$BUILD_DATE
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>Environment: {{ environment }}</div>
+    <div>Version: {{ version }}</div>
+    <div>Build date: {{ build_date }}</div>
+    <div>Tag: {{ tag }}</div>
   </div>
 </template>
+
+<script setup lang="ts">
+const environment = process.env.VUE_APP_SITE || 'Undefined'
+const build_date = process.env.VUE_APP_BUILD_DATE || 'Local development'
+const version = process.env.VUE_APP_VERSION || 'Local development'
+const tag = process.env.VUE_APP_SHA1 || 'Local development'
+</script>
